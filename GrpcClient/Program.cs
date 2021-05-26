72fc68a4a53d4116358a85a4416f86596203a955
YimiaoOu <32688730+YimiaoOu@users.noreply.github.com>
2021-05-25 21:23:22 -0400

    initial commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var input = new HelloRequest { Name = "Amy" };
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+            //var reply = await client.SayHelloAsync(input);
+            //Console.WriteLine(reply.Message);
+
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var studentClient = new Student.StudentClient(channel);
+            var studentRequested = new StudentLookupModel { StudentId = 2 };
+            var student = await studentClient.GetStudentInfoAsync(studentRequested);
+            Console.WriteLine($"{ student.FirstName } { student.LastName }: { student.Email }");
+
+            Console.WriteLine();
+
+            using (var call = studentClient.GetNewStudents(new NewStudentsRequest()))
+            {
+                while (await call.ResponseStream.MoveNext())
+                {
+                    var currentStudent = call.ResponseStream.Current;
+                    Console.WriteLine($"{ currentStudent.FirstName } { currentStudent.LastName}: { currentStudent.Age }, {currentStudent.IsRegistered }");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcServer/Services/StudentsService.cs
@@ -0,0 +1,84 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class StudentsService : Student.StudentBase
+    {
+        private readonly ILogger<StudentsService> _logger;
+
+        public StudentsService(ILogger<StudentsService> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public override Task<StudentModel> GetStudentInfo(StudentLookupModel request, ServerCallContext context)
+        {
+            StudentModel output = new StudentModel();
+            if (request.StudentId == 1)
+            {
+                output.FirstName = "Amy";
+                output.LastName = "Ou";
+                output.Email = "amy@amy.com";
+            }
+            else if (request.StudentId == 2)
+            {
+                output.FirstName = "Leo";
+                output.LastName = "King";
+                output.Email = "leo@king.net";
+            }
+            else
+            {
+                output.FirstName = "Sunny";
+                output.LastName = "Lee";
+                output.Email = "sunny@lee.com";
+            }
+
+            return Task.FromResult(output);
+        }
+
+
+        public override async Task GetNewStudents(NewStudentsRequest request, IServerStreamWriter<StudentModel> responseStream, ServerCallContext context)
+        {
+            List<StudentModel> students = new List<StudentModel> {
+                new StudentModel
+                {
+                    FirstName = "Larry",
+                    LastName = "Kong",
+                    Email = "larry@kong.com",
+                    IsRegistered = true,
+                    Age = 22
+
+                },
+                new StudentModel
+                {
+                    FirstName = "Lian",
+                    LastName = "May",
+                    Email = "lian@may.net",
+                    IsRegistered = false,
+                    Age = 20
+                },
+                new StudentModel
+                {
+                    FirstName = "Scott",
+                    LastName = "Smith",
+                    Email = "scott@smith.com",
+                    IsRegistered = true,
+                    Age = 19
+                }
+            };
+
+            foreach (var student in students)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(student);
+            }
+
+        }
+    }
+}
